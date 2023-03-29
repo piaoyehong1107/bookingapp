cfd8dc2fd7aa06460d7ca2375df6c62848a6a867
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2023-03-29 00:22:55 -0500

    rountes,models

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import roomsRoute from "./routes/rooms.js";
+import hotelsRoute from "./routes/hotels.js";
 
 const app = express();
 dotenv.config();
