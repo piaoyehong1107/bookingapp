45bf44fbebcf71ab0ca93643a4d218b90b4f60d5
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2023-04-05 00:17:48 -0500

    encrypt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,14 +1,35 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
     await newUser.save();
     res.status(200).send("User has been created");
+  } catch (err) {
+    next(err);
+  }
+};
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found!"));
+
+    const isPasswordCorret = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorret)
+      return next(createError(400, "Wrong password or Username!"));
+
+    res.status(200).json(user);
   } catch (err) {
     next(err);
   }
