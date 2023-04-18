5e8bf6d73ce20179e1b2dd57528633750f1e27c7
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2023-04-18 00:30:36 -0500

    search

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import "./header.css"
-import { faBed, faCar, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
+import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Header = () => { 
@@ -41,6 +41,20 @@ const Header = () => {
                     Get rewarded for your travels-unlock instant savings of10% or more with a free Lamabooking account.
                 </p>
                 <button className="headerBtn">Sign in / Register</button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faBed} className="headerIcon" />
+                        <input type="text" placeholder="where are you going?" className="headerSearchInput" />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+                        <span className="headerSearchText">date to date</span>
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+                        <span className="headerSearchText">2 adults 2 children 1 room</span>
+                    </div>
+                </div>
             </div>
     </div>
 )
