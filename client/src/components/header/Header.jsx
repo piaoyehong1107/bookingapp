f171c14edebcee0e6fcb7fed03204beb389538d8
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2023-04-20 00:41:48 -0500

    search

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,29 @@
 import "./header.css"
 import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { DateRange } from "react-date-range"
+import { useState } from "react"
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css'
+import { format } from "date-fns"
 
 const Header = () => { 
+    const [openDate, setOpenDate] = useState(false)
+    const [date, setDate] = useState([
+        {
+        startDate: new Date(),
+        endDate: new Date(),
+        key:'selection'
+        }
+    ])
+    const [openOptions, setOpenOptions] = useState(false)
+    const [options, setOptions] = useState(
+        {
+            adult: 1,
+            children: 0,
+            room:1
+        }
+    )
     return (
         <div className="header">
             <div className="headerContainer"><div className="headerList">
@@ -48,11 +69,50 @@ const Header = () => {
                     </div>
                     <div className="headerSearchItem">
                         <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
-                        <span className="headerSearchText">date to date</span>
+                        <span onClick={ ()=>setOpenDate(!openDate)} className="headerSearchText">{`${format(date[0].startDate,"MM/dd/yyyy")} to ${format(date[0].endDate,"MM/dd/yyyy")} `}</span>
+                        {openDate && <DateRange
+                            editableDateInputs={true}
+                            onChange={item => setDate([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={date}
+                            className="date"
+                        />}
                     </div>
                     <div className="headerSearchItem">
                         <FontAwesomeIcon icon={faPerson} className="headerIcon" />
-                        <span className="headerSearchText">2 adults 2 children 1 room</span>
+                        <span className="headerSearchText">{`${options.adult} adult · ${options.children} children · ${options.room} room `}</span>
+                        <div className="options">
+                            <div className="optionItem">
+                                <span className="optionText">Adult</span>
+                                <div className="optionCounter">
+                                    <button className="optionCounterButton"> - </button>
+                                    <span className="optionCounterNumber">1</span>
+                                    <button className="optionCounterButton"> + </button>
+                                </div>
+                                
+                            </div>
+                            <div className="optionItem">
+                                <span className="optionText">Children</span>
+                                <div className="optionCounter">
+                                    <button className="optionCounterButton"> - </button>
+                                    <span className="optionCounterNumber">0</span>
+                                    <button className="optionCounterButton"> + </button>
+                                </div>
+                                
+                            </div>
+                            <div className="optionItem">
+                                <span className="optionText">Room</span>
+                                <div className="optionCounter">
+                                    <button className="optionCounterButton"> - </button>
+                                    <span className="optionCounterNumber">1</span>
+                                    <button className="optionCounterButton"> + </button>
+                                </div>
+                                
+                            </div>
+                    </div>
+                    </div>
+                    <div className="headerSearchItem">
+                        <button className="headerBtn">Search</button>
                     </div>
                 </div>
             </div>
