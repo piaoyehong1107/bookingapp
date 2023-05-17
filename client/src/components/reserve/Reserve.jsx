b17211d5d702662c9fa3b9903cd638d6383ec93f
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2023-05-17 00:20:10 -0500

    booking

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,8 +1,39 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./reserve.css"
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons"
+import useFetch from "../../hooks/useFetch"
+import { useState } from "react"
+import { useContext } from "react"
+import { SearchContext } from "../../context/SearchContext"
 
-const Reserve = ({setOpen,hotelId}) => { 
+const Reserve = ({ setOpen, hotelId }) => { 
+    const [selectedRooms,setSelectedRooms] = useState([])
+    const {data, loading, error} = useFetch(`/hotels/room/${hotelId}`)
+    const {dates} = useContext(SearchContext)
+
+    const getDatesInRange = (startDate, endDate) => {
+        const start = new Date(startDate)
+        const end = new Date(endDate)
+        const date = new Date(start.getTime());
+        
+        let list = []
+        while (date <= end) {
+            list.push(new Date(date).getTime())
+            date.setDate(date.getDate() + 1)
+        }
+            return list
+    }
+
+    console.log(getDatesInRange(dates[0].startDate, dates[0].endDate))
+
+    const handleSelect = (e) => {
+        const checked = e.target.checked
+        const value = e.target.value
+        setSelectedRooms(checked ? [...selectedRooms, value] : selectedRooms.filter(item=> item !== value))
+    }
+    const handleClick = () => {
+        
+    }
     return (
         <div className="reserve">
             <div className="rContainer">
@@ -12,6 +43,28 @@ const Reserve = ({setOpen,hotelId}) => {
                     onClick={()=> setOpen(false)}
                 />
                 <span>Select your rooms:</span>
+                {data.map(item => (
+                    <div className="rItem">
+                        <div className="rItemInfo">
+                            <div className="rTitle">{item.title}</div>
+                            <div className="rDesc">{item.desc}</div>
+                            <div className="rMax">
+                                Max people: <b>{item.maxPeople}</b>
+                            </div>
+                            <div className="rPrice">{item.price}</div>
+                        </div>
+                        {item.roomNumbers.map(roomNumber => (
+                            <div className="room">
+                                <lable>{roomNumber.numer}</lable>
+                                <input
+                                    type="checkbox"
+                                    value={roomNumber._id}
+                                    onChange={handleSelect} />
+                            </div>
+                        ))}
+                    </div>
+                ))}
+                <button onClick={handleClick} className="rButton">Reserve Now!</button>
             </div>
         </div>
     )
--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -28,7 +28,6 @@ const Hotel = () => {
 
     const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24
     function dayDifference(date1, date2) { 
-        console.log(date1.getTime())
         const timeDiff = Math.abs(date2.getTime() - date1.getTime())
         const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY)
         return diffDays
